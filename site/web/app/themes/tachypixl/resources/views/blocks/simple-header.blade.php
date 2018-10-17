167b7d7e2b816e23629c4b09dcc52116abb16cd7
Kelly Mears <code@kellymears.me>
2018-10-17 09:54:35 -0700

    fix

--- site/web/app/themes/tachypixl/resources/views/blocks/simple-header.blade.php
+++ site/web/app/themes/tachypixl/resources/views/blocks/simple-header.blade.php
@@ -8,7 +8,7 @@
 </div>
 @endif
 <header class="ph0-l pt7 pb0 ph4-l vh-100 header">
-  <div class="mw8 pa4 mt6 pt7 pb6 white ml-auto mr-auto">
+  <div class="mw8 pa4 mt6-l pt7-l pt7-m pt6 pb6 white ml-auto mr-auto">
     <h1 class="white"><a href="{{ home_url('/') }}" alt="{{ get_bloginfo('name', 'display') }}">@svg ( 'logo-horizontal' )</a><br/>
     <span class="code f3 fw1 ttu lh-solid white">Translating Powerful Ideas Into Mainstream Discourse</h1>
   </div>
