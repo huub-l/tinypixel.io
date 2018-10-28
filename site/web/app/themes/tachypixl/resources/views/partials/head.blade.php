9cf617a5cf3706347782404cc2be13099aef8182
Kelly Mears <code@kellymears.me>
2018-10-28 09:50:51 -0700

    development

--- site/web/app/themes/tachypixl/resources/views/partials/head.blade.php
+++ site/web/app/themes/tachypixl/resources/views/partials/head.blade.php
@@ -3,4 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+  <script src="http://localhost:8098"></script>
 </head>
