13dc60cf3f09e2badd01d167bde32cef607dacc5
Kelly Mears <code@kellymears.me>
2018-12-08 12:45:24 -0800

    fix for webpack

--- site/web/app/themes/pixelize/resources/assets/build/webpack.config.js
+++ site/web/app/themes/pixelize/resources/assets/build/webpack.config.js
@@ -7,7 +7,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const CopyGlobsPlugin = require('copy-globs-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
 
 const desire = require('./util/desire');
 const config = require('./config');
@@ -52,10 +51,6 @@ let webpackConfig = {
         loader: 'import-glob',
       },
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
         test: /\.js$/,
         exclude: [/node_modules(?![/|\\](bootstrap|foundation-sites))/],
         use: [
@@ -63,14 +58,6 @@ let webpackConfig = {
           { loader: 'buble', options: { objectAssign: 'Object.assign' } },
         ],
       },
-      {
-        test: /\.css$/,
-        exclude: config.paths.assets,
-        use: [
-          { loader: 'vue-style-loader' },
-          { loader: 'css-loader' },
-        ],
-      }, 
       {
         test: /\.css$/,
         include: config.paths.assets,
@@ -138,9 +125,6 @@ let webpackConfig = {
       config.paths.assets,
       'node_modules',
     ],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-    },
     enforceExtension: false,
   },
   resolveLoader: {
@@ -180,7 +164,6 @@ let webpackConfig = {
       debug: config.enabled.watcher,
       stats: { colors: true },
     }),
-    new VueLoaderPlugin(),
     new webpack.LoaderOptionsPlugin({
       test: /\.s?css$/,
       options: {
