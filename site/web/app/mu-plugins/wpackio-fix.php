0ac5b2430ca20be796de06692a5fe6bc4d992d62
Kelly Mears <code@kellymears.me>
2018-12-08 18:54:48 -0800

    build attempt with explicit libpng-dev dependency installation during build step

--- site/web/app/mu-plugins/wpackio-fix.php
+++ site/web/app/mu-plugins/wpackio-fix.php
@@ -1,18 +1,22 @@
 <?php
-add_action( 'rest_api_init', function() {
-	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-	add_filter( 'rest_pre_serve_request', function( $value ) {
-		header( 'Access-Control-Allow-Origin: *' );
-		header( 'Access-Control-Allow-Methods: GET' );
-		header( 'Access-Control-Allow-Credentials: true' );
-		header( 'Access-Control-Expose-Headers: Link', false );
-		header( 'Access-Control-Allow-Headers: X-Requested-With' );
-		return $value;
+
+if(!\WP_ENV === 'production' || 'staging') {
+	add_action( 'rest_api_init', function() {
+		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+		add_filter( 'rest_pre_serve_request', function( $value ) {
+			header( 'Access-Control-Allow-Origin: *' );
+			header( 'Access-Control-Allow-Methods: GET' );
+			header( 'Access-Control-Allow-Credentials: true' );
+			header( 'Access-Control-Expose-Headers: Link', false );
+			header( 'Access-Control-Allow-Headers: X-Requested-With' );
+			return $value;
+		} );
+	}, 15 );
+
+	add_action( 'customize_preview_init', function() {
+		add_filter( 'wp_headers', function( $headers ) {
+			$headers['Content-Security-Policy'] .= ' 192.168.1.144:3000';
+			return $headers;
+		} );
 	} );
-}, 15 );
-add_action( 'customize_preview_init', function() {
-	add_filter( 'wp_headers', function( $headers ) {
-		$headers['Content-Security-Policy'] .= ' 192.168.1.144:3000';
-		return $headers;
-	} );
-} );
+}
